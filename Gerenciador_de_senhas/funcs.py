252e54185a6211f1853ab0f451c03c3d3ad77866
bmuller70 <bmuller7000@gmail.com>
2023-10-16 19:34:00 -0300

    Solucção json

--- Gerenciador_de_senhas/funcs.py
+++ Gerenciador_de_senhas/funcs.py
@@ -1,16 +1,52 @@
 
 import json,bcrypt,cryptography
+import Gerador_senha
+
 
 
 class Gerenciador():
+    def __init__(self):
+        self.senha = Gerador_senha.senha_f()
+        with open('pass.json', 'r') as j_son:
+            try:
+                self.lista = json.load(j_son)
+            except json.decoder.JSONDecodeError as erro:
+                if erro == "Expecting value: line 1 column 1 (char 0)":
+                    print('isso ai')
+                else:
+                    print(erro)
+            # raise
+            # self.lista = {}
 
     def criar(self):
+        creds = {'nome': '', 'login': ''}
         print('Digite as informações solicitadas')
-        creds = {'nome': '', 'login': '', 'senha': ''}
         for i in creds:
             creds[i] = input(f'{i}: ')
+        creds['senha'] = self.senha
         return creds
 
+    def salvar(self, creds):
+        self.lista["senhas"].append(creds)
+        create = open('pass.json', 'w')
+        json.dump(self.lista, create, indent=4)
+
+
+
+    def alterar(self):
+        try:
+            print(self.escrita)
+        except:
+            pass
+    #def remover(self):
+
+# -----------------------------------------
+# Teste para criação das senhas
 teste = Gerenciador()
-cadastro = teste.criar()
-print(cadastro)
+#cadastro = teste.criar()
+#teste.alterar()
+creds = teste.criar()
+teste.salvar(creds)
+#print(teste.alterar())
+#-----------------------------------------
+
